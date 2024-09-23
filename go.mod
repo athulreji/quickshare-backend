0256145480187bb438c1de421fdfdb5702cdc556
Athul Reji <athulreji007@gmail.com>
2024-09-24 01:22:19 +0530

    Implemented all apis

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module quickshare-backend
 go 1.23.1
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.30.5
 	github.com/aws/aws-sdk-go-v2/config v1.27.33
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.61.2
 	github.com/lib/pq v1.10.9
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.5 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.4 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.32 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.13 // indirect
--- internal/services/presigned_url.go
+++ internal/services/presigned_url.go
@@ -5,61 +5,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
-
-type Presigner struct {
-	PresignClient *s3.PresignClient
-}
-
-// GetObject makes a presigned request that can be used to get an object from a bucket.
-// The presigned request is valid for the specified number of seconds.
-func (presigner Presigner) GetObject(
-	bucketName string, objectKey string, lifetimeSecs int64) (*v4.PresignedHTTPRequest, error) {
-	request, err := presigner.PresignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	}, func(opts *s3.PresignOptions) {
-		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
-	})
-	if err != nil {
-		log.Printf("Couldn't get a presigned request to get %v:%v. Here's why: %v\n",
-			bucketName, objectKey, err)
-	}
-	return request, err
-}
-
-// PutObject makes a presigned request that can be used to put an object in a bucket.
-// The presigned request is valid for the specified number of seconds.
-func (presigner Presigner) PutObject(
-	bucketName string, objectKey string, lifetimeSecs int64) (*v4.PresignedHTTPRequest, error) {
-	request, err := presigner.PresignClient.PresignPutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	}, func(opts *s3.PresignOptions) {
-		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
-	})
-	if err != nil {
-		log.Printf("Couldn't get a presigned request to put %v:%v. Here's why: %v\n",
-			bucketName, objectKey, err)
-	}
-	return request, err
-}
-
-// DeleteObject makes a presigned request that can be used to delete an object from a bucket.
-func (presigner Presigner) DeleteObject(bucketName string, objectKey string) (*v4.PresignedHTTPRequest, error) {
-	request, err := presigner.PresignClient.PresignDeleteObject(context.TODO(), &s3.DeleteObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	})
-	if err != nil {
-		log.Printf("Couldn't get a presigned request to delete object %v. Here's why: %v\n", objectKey, err)
-	}
-	return request, err
-}
 
 // curl -X PUT --upload-file ./your-file.txt "PRESIGNED_URL"
 func GeneratePresignedPutUrl(fileId string) (string, error) {
@@ -71,14 +19,14 @@ func GeneratePresignedPutUrl(fileId string) (string, error) {
 	s3Client := s3.NewFromConfig(cfg)
 	presignClient := s3.NewPresignClient(s3Client)
 
-	presigner := Presigner{
-		PresignClient: presignClient,
-	}
-
 	bucketName := "quickshare-s3"
 	objectKey := fileId
 
-	presignedRequest, err := presigner.PutObject(bucketName, objectKey, 300) // URL valid for 5 minutes
+	presignedRequest, err := presignClient.PresignPutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: &bucketName,
+		Key:    &objectKey,
+	}, s3.WithPresignExpires(5*time.Minute)) // 5 minutes expiry
+
 	if err != nil {
 		log.Fatalf("Failed to generate presigned URL: %v", err)
 	}
@@ -96,14 +44,14 @@ func GeneratePresignedGetUrl(fileId string) (string, error) {
 	s3Client := s3.NewFromConfig(cfg)
 	presignClient := s3.NewPresignClient(s3Client)
 
-	presigner := Presigner{
-		PresignClient: presignClient,
-	}
-
 	bucketName := "quickshare-s3"
 	objectKey := fileId
 
-	presignedRequest, err := presigner.GetObject(bucketName, objectKey, 300) // URL valid for 5 minutes
+	presignedRequest, err := presignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: &bucketName,
+		Key:    &objectKey,
+	}, s3.WithPresignExpires(5*time.Minute)) // 5 minutes expiry
+
 	if err != nil {
 		log.Fatalf("Failed to generate presigned URL: %v", err)
 	}
